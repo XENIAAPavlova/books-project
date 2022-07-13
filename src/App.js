2322d4c104f175351df2f437c903d7ef4541179f
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-07-13 15:59:51 +0200

    Created App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-// import logo from "./logo.svg";
+// import logo from "./Bookverse_logo.svg";
 import "./App.css";
 
 function App() {
-  return "Hello";
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Bookverse App</h1>
+      </div>
+      <footer>
+        This is an open-source code by{" "}
+        <a
+          href="https://www.linkedin.com/in/ksenya-pavlova-k/"
+          target="blank"
+          rel="noreferrer"
+        >
+          Xenia Pavlova
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
