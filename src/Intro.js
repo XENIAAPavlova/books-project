a2b19bc70f1d68f25a9adad1133bccb7e4678c2d
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-07-27 17:22:07 +0200

    Added button

--- src/Intro.js
+++ src/Intro.js
@@ -8,17 +8,22 @@ export default function Intro() {
         <div className="card shadow rounded-2 mb-5 mt-5 border-0">
           <div className="row justify-content-center">
             <div className="col-lg-6">
-              <div className="card-body text-start lh-lg">
-                <h5 className="card-title pt-5">Build your library</h5>
-                <p className="card-text">
+              <div className="card-body text-start">
+                <h5 className="card-title pt-5 mb-4">Build your library</h5>
+                <p className="card-text lh-base fs-6">
                   Add your favorite books directly to lists without having to
                   search for them. This is a great way to keep tabs on what
                   you’ve already read vs. what you hope to read in the future.
                   And, yes, it’s free!
                 </p>
-                <p className="card-text">
-                  <small className="text-muted">Last updated 3 mins ago</small>
-                </p>
+                <div className="card-button">
+                  <button
+                    type="submit"
+                    className="btn btn-light shadow-sm justify-content-md-end border-0 mt-3"
+                  >
+                    Search
+                  </button>
+                </div>
               </div>
             </div>
             <div className="col-lg-5">
