f0d0beaab2c2326bb390b632a6aa769b6777a001
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-08-09 15:55:03 +0200

    Integrated API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React, { useState } from "react";
 import logo from "./Bookverse_logo.svg";
 import "./App.css";
 import Explore from "./Explore";
+// import "./Explore.css";
 import Intro from "./Intro";
 import Random from "./Random";
+import BookResults from "./BookResults";
+import axios from "axios";
 
 export default function App() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState([]);
+
+  function handleResponse(response) {
+    console.log(response.data.docs);
+    setResults(response.data.docs);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    let apiUrl = `https://openlibrary.org/search.json?q=${keyword}&_facet=false&_spellcheck_count=0&limit=10&fields=key,cover_i,title,subtitle,author_name,name&mode=everything`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSearchwordChange(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light ps-5 pe-5 mt-3">
@@ -77,10 +100,33 @@ export default function App() {
       <div className="container">
         <br />
         <section>
-          <Explore />
+          <div className="Search mt-4 mb-4 ms-5">
+            <form onSubmit={search} className="row justify-content-center">
+              <div className="col-6">
+                <input
+                  className="form-control shadow-sm"
+                  type="search"
+                  onChange={handleSearchwordChange}
+                />
+              </div>
+              <div className="col-2">
+                <div className="search-button">
+                  <button
+                    type="submit"
+                    className="btn shadow-sm justify-content-md-end"
+                  >
+                    Search
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </section>
         <section>
           <Intro />
+        </section>
+        <section>
+          <BookResults results={results} />
         </section>
         <p className="random-text">Random picks</p>
         <section>
