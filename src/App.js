dc5bf49bf177225cf49361bdcde6ab533def23f1
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-08-10 16:51:54 +0200

    Added new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import logo from "./Bookverse_logo.svg";
-import "./App.css";
-import Explore from "./Explore";
-// import "./Explore.css";
-import Intro from "./Intro";
-import Random from "./Random";
-import BookResults from "./BookResults";
 import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Intro from "./Intro";
+import BookResults from "./BookResults";
+import Trending from "./Trending";
+import MyCollection from "./MyCollection";
+import Navigation from "./Navigation";
+
+import "./App.css";
+import logo from "./Bookverse_logo.svg";
 
 export default function App() {
   let [keyword, setKeyword] = useState("");
@@ -30,55 +33,7 @@ export default function App() {
 
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light ps-5 pe-5 mt-3">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img src={logo} alt="logo" width="75" height="75" />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav mx-auto">
-              <li className="nav-item">
-                <a className="nav-link px-5" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link px-5" href="#">
-                  Trending
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle px-5"
-                  href="#"
-                  id="navbarDropdownMenuLink"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  My Collection
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link px-5" href="#">
-                  Quotes
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navigation />
       <div className="container">
         <br />
         <section>
