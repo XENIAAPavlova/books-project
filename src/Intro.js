4e06e15ec51d40cd7223ffd8cd320ac36d5c829f
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-08-10 16:52:35 +0200

    Changed styling

--- src/Intro.js
+++ src/Intro.js
@@ -5,7 +5,7 @@ export default function Intro() {
   return (
     <div className="row justify-content-center">
       <div className="col-12">
-        <div className="card shadow rounded-2 mb-5 mt-5 border-0">
+        <div className="card shadow rounded-2 mt-5 border-0">
           <div className="row justify-content-center">
             <div className="col-lg-6">
               <div className="card-body text-start">
