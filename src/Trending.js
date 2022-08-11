8cf38aaa83dcb2a6eb07c52e47ebadbac5e8dcdf
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-08-11 15:02:57 +0200

    Integrated API into Trending component

--- src/Trending.js
+++ src/Trending.js
@@ -1,50 +1,73 @@
-import React from "react";
-import Explore from "./Explore";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Trending.css";
 
 export default function Trending() {
-  return (
-    <div className="container ">
-      <section>
-        <Explore />
-      </section>
-      <div className="card mb-3 mt-5">
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img src="..." className="img-fluid rounded-start" alt="..." />
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <div className="row">
-                <div className="col-5">
-                  <button
-                    id="trending-button"
-                    type="submit"
-                    className="btn shadow-sm justify-content-md-end"
-                  >
-                    Learn more
-                  </button>
+  const [ready, setReady] = useState(false);
+  const [results, setResults] = useState([]);
+
+  function handleApiResponse(response) {
+    console.log(response.data.works);
+    setResults(response.data.works);
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="container ">
+        {results.slice(0, 10).map(function (book, i) {
+          console.log("hello");
+          console.log(book);
+
+          let cover = "/no_cover_pic.png";
+          if (book.cover_i) {
+            cover = `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`;
+          }
+
+          return (
+            <div className="card mb-3 mt-5" key={i}>
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img
+                    src={cover}
+                    className="img-fluid rounded-start"
+                    alt="..."
+                  />
                 </div>
-                <div className="col-5">
-                  <button
-                    id="trending-button"
-                    type="submit"
-                    className="btn shadow-sm justify-content-md-end"
-                  >
-                    Add
-                  </button>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">{book.author_name}</h5>
+                    <p className="card-text">{book.title}</p>
+                    <div className="row">
+                      <div className="col-5">
+                        <button
+                          id="trending-button"
+                          type="submit"
+                          className="btn shadow-sm justify-content-md-end"
+                        >
+                          Learn more
+                        </button>
+                      </div>
+                      <div className="col-5">
+                        <button
+                          id="trending-button"
+                          type="submit"
+                          className="btn shadow-sm justify-content-md-end"
+                        >
+                          Add
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
-    </div>
-  );
+    );
+  } else {
+    let url = "https://openlibrary.org/trending/now.json";
+    axios.get(url).then(handleApiResponse);
+  }
 }
