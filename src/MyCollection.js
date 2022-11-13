252e6c7d59b911d9c0382140b6e17c486cd0db85
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-11-13 16:43:06 +0100

    Rendered "My Collection" page

--- src/MyCollection.js
+++ src/MyCollection.js
@@ -1,63 +1,37 @@
 import React from "react";
 import "./MyCollection.css";
 
-export default function MyCollection() {
-  return (
-    <div className="container">
-      <div className="col-md-3">
-        <div className="card-body mb-5 mt-5 text-center">
-          <img src="#" id="card-top" className="card-img-top" alt="..." />
-          <p className="card-book mb-2 mt-4">Book title</p>
-          <p className="card-author mt-3">Author</p>
-          <div className="col-md-12 text-center d-flex">
-            {/* <a
-              href="#"
-              target="_blank"
-              rel="noreferrer"
-              className="learn-more-button btn mb-3"
-              type="button"
-            >
-              Learn more
-            </a> */}
+export default function MyCollection(props) {
+  if (props.results) {
+    return (
+      <div className="container">
+        <div className="col-md-3">
+          <div className="card-body mb-5 mt-5 text-center">
+            <img src="#" id="card-top" className="card-img-top" alt="..." />
+            {props.results.map(function (book, i) {
+              let author = [];
+              if (book.author_name) {
+                author = book.author_name;
+              }
+              return (
+                <div key={i}>
+                  <p className="card-book mb-2 mt-4">{book.title}</p>
+                  <p className="card-author mt-3">{author}</p>
+                  <div className="col-md-12 text-center d-flex"></div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
-      <span>
-        <nav aria-label="Page navigation example">
-          <ul className="pagination justify-content-center">
-            <li className="page-item disabled">
-              {/* <a
-                className="page-link"
-                href="#"
-                tabIndex="-1"
-                aria-disabled="true"
-              >
-                Previous
-              </a> */}
-            </li>
-            <li className="page-item">
-              {/* <a className="page-link" href="#">
-                1
-              </a> */}
-            </li>
-            <li className="page-item">
-              {/* <a className="page-link" href="#">
-                2
-              </a> */}
-            </li>
-            <li className="page-item">
-              {/* <a className="page-link" href="#">
-                3
-              </a> */}
-            </li>
-            <li className="page-item">
-              {/* <a className="page-link" href="#">
-                Next
-              </a> */}
-            </li>
-          </ul>
-        </nav>
-      </span>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="container">
+        <div className="text-center">
+          <p>Your collection is empty for now</p>
+        </div>
+      </div>
+    );
+  }
 }
