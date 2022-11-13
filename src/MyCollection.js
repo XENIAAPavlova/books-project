7fae9a1366a72f87ee929d2eae3ebb67fe144ec3
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-11-13 17:10:26 +0100

    Changed the appearance of My Collection + added buttons

--- src/MyCollection.js
+++ src/MyCollection.js
@@ -5,23 +5,53 @@ export default function MyCollection(props) {
   if (props.results) {
     return (
       <div className="container">
-        <div className="col-md-3">
-          <div className="card-body mb-5 mt-5 text-center">
-            <img src="#" id="card-top" className="card-img-top" alt="..." />
-            {props.results.map(function (book, i) {
-              let author = [];
-              if (book.author_name) {
-                author = book.author_name;
-              }
-              return (
-                <div key={i}>
-                  <p className="card-book mb-2 mt-4">{book.title}</p>
-                  <p className="card-author mt-3">{author}</p>
-                  <div className="col-md-12 text-center d-flex"></div>
+        <div className="row">
+          {props.results.map(function (book, i) {
+            let author = [];
+            if (book.author_name) {
+              author = book.author_name;
+            }
+
+            let cover = "/no_cover_pic.png";
+            if (book.cover_i) {
+              cover = `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`;
+            }
+            return (
+              <div className="col-md-3" key={i}>
+                <div className="card-body mb-5 mt-5 text-center">
+                  <div>
+                    <div className="">
+                      <img
+                        id="random-book"
+                        src={cover}
+                        className="img-fluid rounded-start"
+                        alt="..."
+                      />
+                    </div>
+                    <p className="card-book mb-2 mt-4">{book.title}</p>
+                    <p className="card-author mt-3">{author}</p>
+                    <div className="col-md-12 text-center">
+                      <button
+                        className="add-button btn mb-3 me-2"
+                        type="button"
+                      >
+                        Remove
+                      </button>
+                      <a
+                        href={`https://openlibrary.org/${book.key}`}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="learn-more-button btn mb-3"
+                        type="button"
+                      >
+                        Read
+                      </a>
+                    </div>
+                  </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
